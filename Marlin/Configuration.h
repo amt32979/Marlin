7fba2664922a1493146489eaf02e68318f3cc0d0
amt32979 <123581442+amt32979@users.noreply.github.com>
2023-02-11 14:16:53 -0600

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1222,7 +1222,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 79.80, 80.24, 823.89, 130 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 79.80, 80.24, 803.4, 130 }
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
